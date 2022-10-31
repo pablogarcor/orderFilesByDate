b0ce845c375c28fa504710d22efaaee3734724ba
pablogarcor <pabgarort1@alum.us.es>
2022-10-31 20:55:28 +0100

    feat: include input of dir path and only run if is a dir path

--- src/main.py
+++ src/main.py
@@ -11,11 +11,6 @@ import shutil
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 def get_files_route_with_mod_date(directory):
     date_regex = re.compile(r'\d\d\d\d-\d\d-\d\d')
     directory_files = os.listdir(directory)
@@ -37,22 +32,30 @@ def get_files_route_with_mod_date(directory):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     date_regex = re.compile(r'\d\d\d\d-\d\d-\d\d')
-    dir_name = os.path.abspath('../test')
-    file_list = get_files_route_with_mod_date(dir_name)
-    for r in file_list:
-        date_dir_name = '/'.join([dir_name, r])
-        try:
-            os.mkdir(date_dir_name)
-        except FileExistsError as e:
-            print("os.mkdir error:", e.args[0])
-        for file in file_list[r]:
-            source_file = '/'.join([dir_name, file['file_name']])
-            if ((not re.search(date_regex, file['file_name'])) and os.path.isdir(source_file)) or os.path.isfile(source_file):
-                destination_file = '/'.join([dir_name, r, file['file_name']])
+    dir_name = ""
+    while not os.path.isdir(dir_name):
+        dir_name = input("Please enter the absolute dir path: ")
+        dir_name = os.path.abspath(dir_name)
+        if not os.path.isdir(dir_name):
+            print(f"The path: {dir_name} is not a directory")
+        else:
+            print(f"{dir_name} is a dir path")
+            file_list = get_files_route_with_mod_date(dir_name)
+            for r in file_list:
+                date_dir_name = '/'.join([dir_name, r])
                 try:
-                    shutil.move(source_file, destination_file)
-                except shutil.Error as e:
-                    print('shutil.move error:', e.args[0])
+                    os.mkdir(date_dir_name)
+                except FileExistsError as e:
+                    print("os.mkdir error:", e.args[0])
+                for file in file_list[r]:
+                    source_file = '/'.join([dir_name, file['file_name']])
+                    if ((not re.search(date_regex, file['file_name'])) and os.path.isdir(
+                            source_file)) or os.path.isfile(source_file):
+                        destination_file = '/'.join([dir_name, r, file['file_name']])
+                        try:
+                            shutil.move(source_file, destination_file)
+                        except shutil.Error as e:
+                            print('shutil.move error:', e.args[0])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
